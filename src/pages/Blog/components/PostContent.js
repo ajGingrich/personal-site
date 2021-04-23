b2b582df7fbb9cb801af0461c400bc1225826c3d
Andrew Gingrich <gandrew320@gmail.com>
2021-04-23 09:16:05 -0600

    import specific languages and highlight blog post

--- src/pages/Blog/components/PostContent.js
+++ src/pages/Blog/components/PostContent.js
@@ -1,42 +1,44 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import PropTypes from 'prop-types';
-import hljs from 'highlight.js';
+import hljs from 'highlight.js/lib/core';
 import DOMPurify from 'dompurify';
 
-// <div dangerouslySetInnerHTML={{ __html: cleanBody }} />
-
+// languages
+import javascript from 'highlight.js/lib/languages/javascript';
+import python from 'highlight.js/lib/languages/python';
+import java from 'highlight.js/lib/languages/java';
+import bash from 'highlight.js/lib/languages/bash';
+import c from 'highlight.js/lib/languages/c';
 import 'highlight.js/styles/atom-one-dark.css';
+
+import styles from '../blog.module.css';
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('python', python);
+hljs.registerLanguage('bash', bash);
+hljs.registerLanguage('java', java);
+hljs.registerLanguage('c', c);
 
 const PostContent = ({ body }) => {
   const cleanBody = DOMPurify.sanitize(body);
-  const node = useRef(null);
+
+  const highlightCode = useCallback(() => {
+    const nodes = document.querySelectorAll('pre code');
+    nodes.forEach(n => {
+      hljs.highlightElement(n);
+    });
+  }, []);
 
   useEffect(() => {
-    // highlightCode();
-    // const nodes = ReactDOM.findDOMNode(this).querySelectorAll('pgn');
-    hljs.highlightElement(node.current)
-  });
+    highlightCode();
+  }, [highlightCode]);
 
   return (
-    <div ref={node}>
-      <pre className="language-python">
-        <code>
-        @requires_authorization(roles=["ADMIN"])
-        def somefunc(param1='', param2=0):
-            r'''A docstring'''
-            if param1 > param2: # interesting
-                print 'Gre\'ater'
-            return (param2 - param1 + 1 + 0b10l) or None
-
-        class SomeClass:
-            pass
-
-        >>> message = '''interpreter
-        ... prompt'''
-        </code>
-      </pre>
-    </div>
-  )
+    <div
+      className={styles.postContent}
+      dangerouslySetInnerHTML={{ __html: cleanBody }}
+    />
+  );
 };
 
 PostContent.propTypes = {
