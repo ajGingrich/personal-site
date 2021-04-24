7ea4238f10a50d54249a52a157765d3f873c416a
Andrew Gingrich <gandrew320@gmail.com>
2021-04-24 12:37:51 -0600

    gzip

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,4 +1,5 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const config = require('./webpack.config');
@@ -6,8 +7,15 @@ const config = require('./webpack.config');
 config.mode = 'production';
 config.module.rules[1].use.unshift(MiniCssExtractPlugin.loader);
 
-config.plugins.unshift(new MiniCssExtractPlugin({
-  filename: 'style.bundle.css',
+// TODO: remove bootstrap from bundle? insert: '#some-element',
+config.plugins.unshift(new MiniCssExtractPlugin());
+
+config.plugins.push(new CompressionPlugin({
+  algorithm: 'gzip',
+  test: /\.js$|\.css$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+  minRatio: 1,
+  threshold: 0,
+  deleteOriginalAssets: true,
 }));
 
 // config.plugins.push(new BundleAnalyzerPlugin());
