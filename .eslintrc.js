de5445fb3bb6934c0f33c78322e514791fa60103
Andrew Gingrich <gandrew320@gmail.com>
2021-12-08 13:04:31 -0600

    lint on eslint file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,30 @@
 module.exports = {
   env: {
-      browser: true,
-      es2021: true
+    browser: true,
+    es2021: true,
   },
   extends: [
-      "plugin:react/recommended",
-      "airbnb",
-      "airbnb/hooks"
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
   ],
   parserOptions: {
-      "ecmaFeatures": {
-          "jsx": true
-      },
-      "ecmaVersion": 12,
-      "sourceType": "module"
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
   plugins: [
-      'react',
+    'react',
   ],
   rules: {
-    "arrow-body-style": [0],
-    "arrow-parens": ["error", "as-needed"],
-    "react/jsx-filename-extension": [0],
-    "react/function-component-definition": [0]
+    'arrow-body-style': [0],
+    'arrow-parens': ['error', 'as-needed'],
+    'react/jsx-filename-extension': [0],
+    'react/function-component-definition': [0],
   },
   settings: {
-    "import/resolver": "webpack"
+    'import/resolver': 'webpack',
   },
-}
+};
