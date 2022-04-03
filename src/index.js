a450bae00ad6f780878bc3e470de6668ac38abd7
Andrew Gingrich <gandrew320@gmail.com>
2022-04-02 19:28:37 -0600

    upgrade to react 18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,11 +10,12 @@ import configureStore from './store';
 import Routes from './routes';
 
 const render = Component => {
-  ReactDOM.render(
+  const container = document.getElementById('root');
+  const root = createRoot(container);
+  root.render(
     <Provider store={configureStore()}>
       <Component />
-    </Provider>,
-    document.getElementById('root'),
+    </Provider>
   );
 };
 
